207feabd5c79b5cee885578767c16b6bacb2b6cb
Jose Alonso <mail@josealonso.dev>
2023-03-03 18:20:54 -0600

    Add project files.

---
+++ SSODemoUsers.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSODemoUsers.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
